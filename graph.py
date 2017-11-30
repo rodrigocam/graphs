bf50f63f24cad8a11a8f7c7423d6ad94d710ec44
Rodrigo <rodrigo.redcode@gmail.com>
2017-11-29 22:55:03 -0200

    Add minimum spanning tree prim's algorithm

---
+++ graph.py
@@ -0,0 +1,50 @@
+from pythonds.graphs import Graph, Vertex, PriorityQueue
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def minimum_spanning_tree(graph, initial_city):
+    start_city = graph.getVertex(initial_city)
+    heap = PriorityQueue()
+    aux_list = []
+    solution = []
+    visited_nodes = []
+
+    new_graph = Graph()
+
+    for conect in start_city.getConnections():
+        aux_list.append((start_city.getWeight(conect), (start_city.id, conect.id)))
+        visited_nodes.append(start_city.id)
+
+    heap.buildHeap(aux_list)
+
+    while not heap.isEmpty():
+        cur_pair = heap.delMin()
+        cur_node = graph.getVertex(cur_pair[1])
+        
+        if cur_node not in visited_nodes:
+            solution.append(cur_pair)
+            visited_nodes.append(cur_node.id)
+            for conect in cur_node.getConnections():
+                if conect.id not in visited_nodes:
+                    heap.add((cur_node.getWeight(conect), (cur_node.id, conect.id)))
+        else:
+            continue
+
+    #print(solution)
+
+    for city_1, city_2 in solution:
+        new_graph.addEdge(city_1, city_2, 0)
+
+    return new_graph
+
+def show_graph(graph, graph_name):
+    visible_graph = nx.Graph()
+
+    for city in graph:
+        for conect in city.getConnections():
+            visible_graph.add_edge(city.id, conect.id)
+            visible_graph.add_node(city.id)
+
+    nx.draw_networkx(visible_graph, with_labels=True)
+    plt.savefig(graph_name)
+    plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from pythonds.graphs import Graph
 from collections import namedtuple
+from graph import minimum_spanning_tree, show_graph
 import os
 import time
 
 
-Pair = namedtuple('Pair', 'city_1 city_2 cost')
-graph = []
-city_list = []
+graph = Graph()
 
 def add_road_menu():
     os.system('clear')
@@ -17,33 +17,17 @@ def add_road_menu():
     if option == '1':
         os.system('clear')
 
-        aux_list = []
-
-        print('\nCidades cadastradas: ' + str(city_list))
-
-        for pair in graph:
-            aux_list.append((pair.city_1, pair.city_2))
-
-        print('\nCidades ligadas: ' + str(aux_list))
+        print('\nCidades cadastradas: ' + str(graph.getVertices()))
 
         print('\nDigite o par de cidades ligadas pelo caminho:\n')
         city1 = input('Cidade 1 - ')
         city2 = input('Cidade 2 - ')
-        cost = input('Tamanho do caminho - ')
+        cost = int(input('Tamanho do caminho - '))
 
-        if city1 in city_list and city2 in city_list:
-            print('WHAT')
-            pair = Pair(city_1=city1, city_2=city2, cost=cost)
-            graph.append(pair)
-            add_road_menu()
-        else:
-            print('\nCidades não cadastradas!!!\n')
-            time.sleep(2)
-            add_road_menu()
-
+        graph.addEdge(city1, city2, cost)
+        add_road_menu()
     elif option == '2':
         main_menu()
-    
     else:
         print('\nOpção inválida\n')
         add_road_menu()
@@ -51,8 +35,9 @@ def add_road_menu():
 
 def add_city_menu():
     os.system('clear')
+
     city = input('\nDigite o nome da cidade: ')
-    city_list.append(city)
+    graph.addVertex(city)
 
     print('\n1 - Adicionar mais cidades\n')
     print('\n2 - Voltar para o menu inicial\n')
@@ -65,44 +50,24 @@ def add_city_menu():
         main_menu()
     else:
         print('\nOpção inválida\n')
+        add_city_menu()
 
 
-def remove_city_menu():
-    os.system('clear')
-
-    print('\n1 - Remover cidade\n')
-    print('2 - Voltar para o menu principal\n')
-
-    remove_city = input()
-
-    if option == '1':
-        if remove_city in city_list and remove_city in city_list:
-            city_list.remove(remove_city)
-            i = 0
-            for pair int graph:
-                if graph[i].city_1 == remove_city or graph[i].city_2 == remove_city:
-                    del graph[i]
-                i += 1
-        else:
-            print('\nCidade não cadastrada!!\n')
-            time.sleep(2)
-            remove_city_menu()
-    elif option == '2':
-        main_menu()
-    else:
-        remove_city_menu()
-
-
-
+def generate_minimum_network():
+    # print(graph.vertices)
+    show_graph(graph, 'initial_graph.png')
+    new_graph = minimum_spanning_tree(graph, 'BSB')
+    # print(graph.vertices)
+    show_graph(new_graph, 'minimum_spanning_tree.png')
+    main_menu()
 
 def main_menu():
     os.system('clear')
     print('Escolha uma opção!\n')
     print('1 - Adicionar uma cidade\n')
     print('2 - Adicionar um caminho entre cidades\n')
-    print('3 - Remover uma cidade\n')
-    print('4 - Gerar rede de menor custo\n')
-    print('5 - Sair\n')   
+    print('3 - Gerar rede de menor custo\n')
+    print('4 - Sair\n')   
 
     option = input()
 
@@ -111,10 +76,8 @@ def main_menu():
     elif option == '2':
         add_road_menu()
     elif option == '3':
-        remove_city_menu()
-    elif option == '4':
         generate_minimum_network()
-    elif option == '5':
+    elif option == '4':
         exit()
     else:
         print('\nOpção inválida\n')
